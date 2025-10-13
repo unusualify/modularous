554b214a463fb76859809b4e55ef49a2b7e41c15
celikerde <celikerde@itu.edu.tr>
2025-10-13 14:33:45 +0300

    fix(modules): add 'price_type' entry to English and Turkish language files
    - Added 'price_type' translation to both English and Turkish language files for consistency.
    - Removed duplicate 'price_type' entry to streamline the language files.

--- lang/en/modules.php
+++ lang/en/modules.php
@@ -3,8 +3,8 @@
 return [
     'company' => 'Company | Companies | {n} Companies',
     'permission' => 'Permission | Permissions | {n}  Permissions',
+    'price_type' => 'Price Type | Price Types | {n} Price Types',
     'role' => 'Role | Roles | {n}  Roles',
     'user' => 'User | Users | {n} Users',
     'vat_rate' => 'Vat Rate | Vat Rates | {n} Vat Rates',
-    'price_type' => 'Price Type | Price Types | {n} Price Types',
 ];
