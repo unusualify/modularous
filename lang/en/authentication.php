d196db123c85d4c087d70e1d199839d9dfce316d
celikerde <celikerde@itu.edu.tr>
2025-10-14 00:58:53 +0300

    fix(authentication): update English and Turkish authentication language files
    - Enhanced the English and Turkish authentication language files with additional entries for improved user experience.
    - Added new translations for registration, login, and password management prompts.
    - Removed outdated entries to streamline the language files and ensure consistency across translations.

--- lang/en/authentication.php
+++ lang/en/authentication.php
@@ -2,61 +2,65 @@
 
 return [
     'back-to-login' => 'Back to Login',
+
     'choose-password' => 'Choose password',
+    'company' => 'Company Name',
+    'complete-registration' => 'Complete Registration',
+    'complete-registration-title' => 'COMPLETE REGISTRATION',
+    'conditions-policy' => 'Conditions Policy',
+    'confirm-provider' => 'Confirm your {provider} account',
+    'create-an-account' => 'CREATE AN ACCOUNT',
+
     'email' => 'E-mail address',
+
     'forgot-password' => 'Forgot password',
+
+    'generate-password' => 'Generate Password',
+    'go-to-sign-in' => 'Go to Sign In',
+
+    'have-an-account' => 'Already have an account?',
+
     'login' => 'Login',
-    'name' => 'Name',
+    'login-success-message' => 'Login Successful!',
     'login-title' => 'SIGN IN TO GET STARTED',
+    'logout' => 'Logout',
+    'logout-cancel' => 'Cancel',
+    'logout-confirm' => 'Confirm',
+    'logout-description' => 'Are you sure you want to log out?',
+    'logout-title' => 'Log Out',
+
+    'name' => 'Name',
+
     'oauth-link-title' => 'Re-enter your password to link :provider to your account',
     'otp' => 'One-time password',
+
     'password' => 'Password',
     'password-confirmation' => 'Confirm password',
+    'password-sent' => 'Password is sent',
+    'pre-register-button-text' => 'Go to Sign In',
+    'pre-register-description' => 'Please check your email for the verification link.',
+    'pre-register-title' => 'Verify Your Email',
+
     'register' => 'Register',
+    'register-button-text' => 'Go to Sign In?',
+    'register-description' => 'Congratulations! Your registration is complete.',
+    'register-title' => 'Registration Complete',
+    'repeat-password' => 'Repeat Password',
     'reset-password' => 'Reset password',
     'reset-send' => 'Send password reset link',
-    'surname' => 'Surname',
-    'verify-login' => 'Verify login',
-    'logout' => 'Logout',
-    'login-success-message' => 'Login Successful!',
-    'company' => 'Company Name',
-    'repeat_password' => 'Repeat Password',
-    'tos' => 'I agree with Terms and Conditions.',
-    'create-an-account' => 'CREATE AN ACCOUNT',
-    'have-an-account' => 'Already have an account?',
+
     'sign-in' => 'Sign In',
     'sign-in-oauth' => 'Sign in with :provider',
     'sign-up-oauth' => 'Sign up with :provider',
-
-    'password-sent' => 'Password is sent',
+    'success' => 'Authentication successful',
+    'success-description' => 'You have been successfully authenticated.',
     'success-reset-email' => 'Please check your email for the password reset link.',
-    'go-to-sign-in' => 'Go to Sign In',
-
-    'register-title' => 'Registration Complete',
-    'register-description' => 'Congratulations! Your registration is complete.',
-    'register-button_text' => 'Go to Sign In?',
-
-    'pre-register-title' => 'Verify Your Email',
-    'pre-register-description' => 'Please check your email for the verification link.',
-    'pre-register-button_text' => 'Go to Sign In',
-
-    'generate-password' => 'Generate Password',
-
-    'confirm-provider' => 'Confirm your {provider} account',
-    'complete-registration-title' => 'COMPLETE REGISTRATION',
-    'complete-registration' => 'Complete Registration',
+    'surname' => 'Surname',
 
     'verification-complete' => 'Verification',
     'verification-complete-description' => 'Your email has been verified.',
-
-    'success' => 'Authentication successful',
-    'success-description' => 'You have been successfully authenticated.',
+    'verify-login' => 'Verify login',
 
     'terms-policy' => 'Terms Policy',
-    'conditions-policy' => 'Conditions Policy',
-
-    'logout-title' => 'Log Out',
-    'logout-description' => 'Are you sure you want to log out?',
-    'logout-cancel' => 'Cancel',
-    'logout-confirm' => 'Confirm',
+    'tos' => 'I agree with Terms and Conditions.'
 ];
