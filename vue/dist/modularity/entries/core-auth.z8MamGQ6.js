93831b61abdc6fe42ae9227906b419629e1882e5
OoBook <oguz.bukcuoglu@gmail.com>
2025-11-13 16:53:01 +0300

    build(vue): update build artifacts for v0.51.5

--- vue/dist/modularity/entries/core-auth.z8MamGQ6.js
+++
@@ -1,2 +0,0 @@
-import{af as t}from"../js/vendor.CHHOD9Ru.js";import{a as r,s as p}from"../js/UEConfig.DC6BZCNT.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.BY-o4QAK.js";const s=p,o=t({store:s});o.use(r);o.mount("#auth");
-//# sourceMappingURL=core-auth.z8MamGQ6.js.map
