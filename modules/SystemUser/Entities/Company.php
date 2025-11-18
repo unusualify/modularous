edab9509f8a0d1dc86857abddad991d2179dac9c
OoBook <oguz.bukcuoglu@gmail.com>
2025-11-18 13:52:31 +0300

    fix(Company): add spreadableClass property to extend ModularityCompany functionality

--- modules/SystemUser/Entities/Company.php
+++ modules/SystemUser/Entities/Company.php
@@ -4,4 +4,6 @@ namespace Modules\SystemUser\Entities;
 
 use Unusualify\Modularity\Entities\Company as ModularityCompany;
 
-class Company extends ModularityCompany {}
+class Company extends ModularityCompany {
+    public static $spreadableClass = \Unusualify\Modularity\Entities\Company::class;
+}
