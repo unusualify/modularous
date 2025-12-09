474635c4025ecbc3b7c5b8510549626cd4475a61
OoBook <oguz.bukcuoglu@gmail.com>
2025-12-10 01:11:09 +0300

    feat(Translations): add new payment-related keys for English and Turkish languages

--- lang/en/payment.php
+++ lang/en/payment.php
@@ -1,9 +1,37 @@
 <?php
 
 return [
+    'company-based-vat-rate' => 'Company Based VAT Rate',
+    'company-based-vat-percentage' => 'Company Based VAT Percentage',
+    'company-based-vat-rate-name' => 'Company Based VAT Rate Name',
+    'company-type' => 'Company Type',
+    'converted-currency' => 'Converted Currency',
+
+    'discount-percentage' => 'Discount Percentage',
+    'discount-amount' => 'Discount Amount',
+
     'error-title' => 'Payment Failed',
+    'exchange-rate' => 'Exchange Rate',
+
     'failed-description' => 'Your payment has been failed. Please try again later or contact with our team.',
+
+    'original-currency' => 'Original Currency',
+
+    'paid_at' => 'Paid At',
+    'paid' => 'Paid',
+    'paid-item' => 'Paid :item',
+    'payment-service' => 'Payment Service',
+
+    'sub-total' => 'Sub Total',
+    'sub-total-paid' => 'Sub Total Paid',
     'success-description' => 'Payment successfully received! 🎉 !',
     'success-title' => 'Payment Complete',
+
+    'total' => 'Total',
+    'total-paid' => 'Total Paid',
+    'transaction-fee-percentage' => 'Transaction Fee Percentage',
+    'transaction-fee-amount' => 'Transaction Fee Amount',
     'transfer-description' => 'Thank you for your payment. When your transfer is completed, you will be informed.',
+
+    'vat-rate-from' => 'VAT Rate From',
 ];
--- lang/tr/payment.php
+++ lang/tr/payment.php
@@ -1,9 +1,37 @@
 <?php
 
 return [
+    'company-based-vat-rate' => 'Şirket Tabanlı KDV Oranı',
+    'company-based-vat-percentage' => 'Şirket Tabanlı KDV Yüzdesi',
+    'company-based-vat-rate-name' => 'Şirket Tabanlı KDV Oranı Adı',
+    'company-type' => 'Şirket Tipi',
+    'converted-currency' => 'Dönüştürülen Para Birimi',
+
+    'discount-percentage' => 'İndirim Yüzdesi',
+    'discount-amount' => 'İndirim Tutarı',
+
     'error-title' => 'Ödeme Başarısız',
+    'exchange-rate' => 'Döviz Kuru',
+
     'failed-description' => 'Ödemeniz başarısız oldu. Lütfen daha sonra tekrar deneyin veya B2Press ekibi ile iletişime geçin.',
+
+    'original-currency' => 'Orijinal Para Birimi',
+
+    'paid_at' => 'Ödeme Tarihi',
+    'paid' => 'Ödendi',
+    'paid-item' => 'Ödenen :item',
+    'payment-service' => 'Ödeme Servisi',
+
+    'sub-total' => 'Ara Toplam',
+    'sub-total-paid' => 'Ödenen Ara Toplam',
     'success-description' => 'Ödeme alındı! 🎉 PR\'ınız üzerinde çalışmaya başlayacağız ve sizi güncel tutacağız.<br>B2Press\'i seçtiğiniz için teşekkürler!',
     'success-title' => 'Ödeme Tamamlandı',
+
+    'total' => 'Toplam',
+    'total-paid' => 'Toplam Ödenen',
+    'transaction-fee-percentage' => 'İşlem Ücreti Yüzdesi',
+    'transaction-fee-amount' => 'İşlem Ücreti Tutarı',
     'transfer-description' => 'Ödemeniz için teşekkürler. Transferiniz tamamlandığında bilgilendirileceksiniz.',
+
+    'vat-rate-from' => 'KDV Kaynağı',
 ];
