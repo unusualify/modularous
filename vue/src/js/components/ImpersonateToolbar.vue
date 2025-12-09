dc32510fce004df02e05d4c4a69cb4f47c46ed53
OoBook <oguz.bukcuoglu@gmail.com>
2025-12-09 18:10:30 +0300

    refactor(ImpersonateToolbar): streamline props definition and enhance template structure for improved clarity

--- vue/src/js/components/ImpersonateToolbar.vue
+++ vue/src/js/components/ImpersonateToolbar.vue
@@ -1,146 +1,118 @@
 <script setup>
-import { watch, ref, computed, nextTick } from 'vue'
+  import { watch, ref, computed, nextTick } from 'vue'
 
-const props = defineProps({
-  modelValue: {
-    type: Boolean,
-    default: false
-  },
-  active: {
-    type: Boolean,
-    default: false
-  },
-  users: {
-    type: Array,
-    default () {
-      return []
+  const props = defineProps({
+    modelValue: {
+      type: Boolean,
+      default: false
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+    users: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    impersonated: {
+      type: Boolean,
+      default: false
+    },
+    route: {
+      type: String,
+      default: '/users/impersonate/:id'
+    },
+    stopRoute: {
+      type: String,
+      default: '/users/impersonate/stop'
+    },
+    fetchEndpoint: {
+      type: String,
+      default: null
+    },
+    itemTitle: {
+      type: String,
+      default: 'name'
+    },
+    itemValue: {
+      type: String,
+      default: 'id'
+    },
+    density: {
+      type: String,
+      default: 'comfortable'
+    },
+    variant: {
+      type: String,
+      default: 'outlined'
     }
-  },
-  impersonated: {
-    type: Boolean,
-    default: false
-  },
-  route: {
-    type: String,
-    default: '/users/impersonate/:id'
-  },
-  stopRoute: {
-    type: String,
-    default: '/users/impersonate/stop'
-  },
-  fetchEndpoint: {
-    type: String,
-    default: null
-  },
-  itemTitle: {
-    type: String,
-    default: 'name'
-  },
-  itemValue: {
-    type: String,
-    default: 'id'
-  },
-  density: {
-    type: String,
-    default: 'comfortable'
-  },
-  variant: {
-    type: String,
-    default: 'outlined'
-  }
-})
-const emit = defineEmits(['update:modelValue'])
+  })
+  const emit = defineEmits(['update:modelValue'])
 
-const selected = ref(null)
+  const selected = ref(null)
 
-const show = computed({
-  get () {
-    return props.modelValue
-  },
-  set (value) {
-    emit('update:modelValue', value)
-  }
-})
+  const show = computed({
+    get () {
+      return props.modelValue
+    },
+    set (value) {
+      emit('update:modelValue', value)
+    }
+  })
 
-watch(selected, function (newValue, oldValue) {
-  if(newValue) {
-    nextTick(() => {
-      window.location.href = impersonateRoute.value
-    })
-  }
-})
+  watch(selected, function (newValue, oldValue) {
+    if(newValue) {
+      nextTick(() => {
+        window.location.href = impersonateRoute.value
+      })
+    }
+  })
 
-const impersonateRoute = computed(() => {
-  return props.route.replace(':id', selected.value)
-})
-
-
+  const impersonateRoute = computed(() => {
+    return props.route.replace(':id', selected.value)
+  })
 </script>
 <template>
-  <v-bottom-sheet v-model="show">
+  <v-list-item
+      v-if="impersonated"
+      class="font-weight-bold mt-2"
+      color="primary"
+      :ripple="false"
+      :append="false"
+      prepend-icon="mdi-account-off"
+      title="Stop Impersonating"
+
+      variant="flat"
+      base-color="red"
+
+      @click="() => window.location.href = stopRoute"
+  />
+  <v-input-browser v-else
+    v-model="selected"
+    :endpoint="fetchEndpoint"
+    :variant="variant"
+    :density="density"
+    :item-title="itemTitle"
+    :item-value="itemValue"
+    >
     <template v-slot:activator="{ props }">
       <v-list-item
-          v-bind="props"
-          class="font-weight-bold mt-2"
-          :ripple="false"
-          :append="false"
-          prepend-icon="mdi-account-switch"
-          title="Impersonate Manager"
-          >
-      </v-list-item>
-
+        @click="props.onClick"
+        class="font-weight-bold"
+        :ripple="false"
+        :append="false"
+        prepend-icon="mdi-account-switch"
+        title="Impersonate User"
+      />
     </template>
-
-    <div class="pa-3 bg-primary">
-      <v-btn v-if="impersonated" color="red" :href="stopRoute">
-          Stop Impersonating
-      </v-btn>
-      <!-- <v-select v-if="!impersonated"
-        class="mt-5"
-        v-model="selected"
-        :items="users"
-        variant="outlined"
-        density="compact"
-        item-title="name"
-        item-value="id"
-        clearable
-        >
-        <template v-slot:item="{ props: itemProps, item }">
-          <v-list-item v-bind="itemProps" :title="`${item.raw.name} (${item.raw.company_name})`" :subtitle="item.raw.email"></v-list-item>
-        </template>
-      </v-select> -->
-      <v-input-browser v-if="!impersonated"
-        class="mt-5"
-        v-model="selected"
-
-        :endpoint="fetchEndpoint"
-        :variant="variant"
-        :density="density"
-        :item-title="itemTitle"
-        :item-value="itemValue"
-        >
-        <template v-slot:item="{ item }">
-          <v-list-item :title="`${item.raw.name} (${item.raw.company_name})`" :subtitle="item.raw.email"></v-list-item>
-        </template>
-      </v-input-browser>
-      <!-- <v-btn v-if="!impersonated" :href="impersonateRoute" :disabled="!selected">Impersonate</v-btn> -->
-    </div>
-  </v-bottom-sheet>
+    <template v-slot:item="{ item }">
+      <v-list-item :title="`${item.raw.name} (${item.raw.company_name})`" :subtitle="item.raw.email" />
+    </template>
+  </v-input-browser>
 
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-
-    }
-  },
-  created () {
-
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 
