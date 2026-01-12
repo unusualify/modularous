99a4274d2737c2cd95759317fab3a0f2d368f9a3
OoBook <oguz.bukcuoglu@gmail.com>
2026-01-12 18:14:35 +0300

    build(vue): update build artifacts for v0.55.0

--- vue/dist/modularity/entries/core-index.2K86izTv.js
+++
@@ -1,2 +0,0 @@
-import{ag as t}from"../js/vendor.LbordFvg.js";import{a as r,s as m}from"../js/UEConfig.CxRaJu4Y.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.nFTu0Umg.js";const p=m,o=t({store:p});o.use(r);o.mount("#admin");
-//# sourceMappingURL=core-index.2K86izTv.js.map
