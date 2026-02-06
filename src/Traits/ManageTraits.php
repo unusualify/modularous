c9a8b6b7199ef01f0b5bfbdffff81cf32d620dc7
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-02-07 01:49:37 +0300

    refactor(ManageTraits): consolidate trait methods and properties, integrating Traitify for enhanced dynamic retrieval

--- src/Traits/ManageTraits.php
+++ src/Traits/ManageTraits.php
@@ -9,50 +9,7 @@ use Unusualify\Modularity\Facades\ModularityFinder;
 
 trait ManageTraits
 {
-    use ManageModuleRoute;
-
-    /**
-     * @return array
-     */
-    protected function traitsMethods(?string $method = null)
-    {
-        $method = $method ?? debug_backtrace()[1]['function'];
-
-        $traits = array_values(class_uses_recursive(get_called_class()));
-
-        $uniqueTraits = array_unique(array_map('class_basename', $traits));
-
-        $methods = array_map(function (string $trait) use ($method) {
-            return $method . $trait;
-        }, $uniqueTraits);
-
-        return array_filter($methods, function (string $method) {
-            return method_exists(get_called_class(), $method);
-        });
-    }
-
-    /**
-     * Get the inputs for the current route.
-     *
-     * @param bool $noGroupChunk
-     * @return array
-     */
-    protected function traitProperties(?string $property = null)
-    {
-        $property = $property ?? debug_backtrace()[1]['function'];
-
-        $traits = array_values(class_uses_recursive(get_called_class()));
-
-        $uniqueTraits = array_unique(array_map('class_basename', $traits));
-
-        $properties = array_map(function (string $trait) use ($property) {
-            return $property . $trait;
-        }, $uniqueTraits);
-
-        return array_filter($properties, function (string $property) {
-            return property_exists(get_called_class(), $property);
-        });
-    }
+    use ManageModuleRoute, Traitify;
 
     public function inputs($noGroupChunk = false)
     {
