56cf385676665e5f445153f92b5742cd5e077850
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-03-09 14:06:51 +0300

    refactor(SpreadHydrate): improve hydration logic and handle reserved keys
    - Enhanced the hydrate method to better manage input properties, including handling of reserved keys and spreadable inputs.
    - Removed unnecessary debugging code and streamlined the logic for setting the 'name' and 'reservedKeys' properties based on module data.
    - Ensured default values are assigned when specific conditions are not met, improving robustness of the input handling.

--- src/Hydrates/Inputs/SpreadHydrate.php
+++ src/Hydrates/Inputs/SpreadHydrate.php
@@ -28,34 +28,42 @@ class SpreadHydrate extends InputHydrate
         $input = $this->input;
         // add your logic
         $input['type'] = 'input-spread';
-        // dd($input);
-        // $input['items'] = ['test'];
+
+        
         if (in_array('scrollable', $input)) {
             $input = array_diff($input, ['scrollable']);
             $input['scrollable'] = true;
 
         }
 
-        $module = Modularity::find($input['_moduleName']);
-        $model = App::make($module->getRouteClass($input['_routeName'], 'model'));
-        // dd($model);
+        if (isset($input['_moduleName']) && isset($input['_routeName'])) {
+            $module = Modularity::find($input['_moduleName']);
+            $model = App::make($module->getRouteClass($input['_routeName'], 'model'));
+            
+            if (! isset($input['reservedKeys'])) {
+                $input['reservedKeys'] = $model->getReservedKeys();
+            }
 
-        if (! isset($input['reservedKeys'])) {
-            $input['reservedKeys'] = $model->getReservedKeys();
+            $spreadableInputs = collect($model->getRouteInputs())
+                ->filter(function ($item) {
+                    return isset($item['spreadable']) && $item['spreadable'] === true;
+                })
+                ->pluck('name');
+
+            if (! empty($spreadableInputs) || $spreadableInputs) {
+                $input['reservedKeys'] = array_merge($input['reservedKeys'], $spreadableInputs->toArray());
+            }
+
+            $input['name'] = $model->getSpreadableSavingKey();
+        } else {
+            if (! isset($input['reservedKeys'])) {
+                $input['reservedKeys'] = [];
+            }
+            if (! isset($input['name'])) {
+                $input['name'] = 'spread_payload';
+            }
         }
-
-        // $allInputs = $model->getRouteInputs();
-        $spreadableInputs = collect($model->getRouteInputs())
-            ->filter(function ($item) {
-                return isset($item['spreadable']) && $item['spreadable'] === true;
-            })
-            ->pluck('name');
-
-        if (! empty($spreadableInputs) || $spreadableInputs) {
-            // dd( array_merge($input['reservedKeys'], $spreadableInputs->toArray()));
-            $input['reservedKeys'] = array_merge($input['reservedKeys'], $spreadableInputs->toArray());
-        }
-
+        
         $input['col'] = [
             'cols' => 12,
             'sm' => 12,
@@ -63,16 +71,6 @@ class SpreadHydrate extends InputHydrate
             'lg' => 12,
             'xl' => 12,
         ];
-
-        $input['name'] = $model->getSpreadableSavingKey();
-        // dd($input['reservedKeys']);
-        // $input['reservedKeys'] = collect($this->module->getRouteInput($input['_routeName']))
-        // ->filter(fn($item) => $item['name'] !== '_spread')
-        // ->pluck('name')
-        // ->toArray();
-
-        // dd($reservedKeys);
-        // dd($input, $this->module, get_class_methods($this->module));
         return $input;
     }
 }
