da4ab10093fcfbd8e8b1a0de62ced376dce915d0
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-03-12 15:40:24 +0300

    refactor(SCSS): remove unused SCSS files and mixins
    - Deleted multiple SCSS files and mixins that are no longer utilized in the project, including wireframe styles, abstract functions, and various component styles.
    - This cleanup enhances maintainability and reduces clutter in the codebase, ensuring a more streamlined styling approach.

--- vue/src/js/mixins/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LocaleMixin } from './locale'
-export { default as ModalMixin } from './modal'
-export { default as InputMixin } from './input'
-export { default as MediaLibraryMixin } from './mediaLibrary/mediaLibrary'
