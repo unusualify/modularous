8d49623a4178410b54d64a3b4091e598a3b34adf
celikerde <celikerde@itu.edu.tr>
2026-05-08 17:11:19 +0300

    refactor(DefaultCountrySeeder, DefaultDatabaseSourceSeeder, DefaultDatabaseUtilitySeeder): enhance country seeder and add new database seeders
    - Added existence check for the countries table in DefaultCountrySeeder to prevent errors during seeding.
    - Replaced Country::truncate() with safer Country::query()->delete() to handle foreign key constraints.
    - Introduced DefaultDatabaseSourceSeeder and DefaultDatabaseUtilitySeeder to streamline database seeding processes by grouping related seeders.

---
+++ database/seeders/DefaultDatabaseSourceSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Unusualify\Modularity\Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Modules\SystemPayment\Database\Seeders\SystemPaymentDatabaseSeeder;
+
+class DefaultDatabaseSourceSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            DefaultRolesSeeder::class,
+            DefaultPermissionsSeeder::class,
+        ]);
+    }
+}
---
+++ database/seeders/DefaultDatabaseUtilitySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Unusualify\Modularity\Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Modules\SystemPayment\Database\Seeders\SystemPaymentDatabaseSeeder;
+
+class DefaultDatabaseUtilitySeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            DefaultCurrencySeeder::class,
+            DefaultVatRateSeeder::class,
+            DefaultPriceTypeSeeder::class,
+            DefaultCountrySeeder::class,
+            SystemPaymentDatabaseSeeder::class,
+        ]);
+    }
+}
